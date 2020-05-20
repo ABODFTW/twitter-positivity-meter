642cc0c72638e2bee6471300d7f2d1c83d484722
Abod <abod80000@gmail.com>
2020-05-20 17:29:58 +0200

    Update settings

--- manage.py
+++ manage.py
@@ -5,9 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "twitter_positvity_meter.settings.production"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitter_positvity_meter.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- twitter_positvity_meter/settings.py
+++ twitter_positvity_meter/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -71,6 +72,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "twitter_positvity_meter.wsgi.application"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -103,3 +110,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+django_heroku.settings(locals())
